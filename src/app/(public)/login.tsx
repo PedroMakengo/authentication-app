a83c2d30bcf6f254d7f1a82bc3702d9324b33ad3
PedroMakengo <pedromakengomiguel@gmail.com>
2024-06-02 14:02:29 +0100

    Criado as telas

--- src/app/(public)/login.tsx
+++ src/app/(public)/login.tsx
@@ -1,9 +1,78 @@
-import { View, Text } from 'react-native'
+import { useSignIn } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
+import { useEffect, useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Pressable,
+} from 'react-native'
+import Spinner from 'react-native-loading-spinner-overlay'
 
 export default function Login() {
+  const { signIn, setActive, isLoaded } = useSignIn()
+
+  const [loading, setLoading] = useState(false)
+  const [emailAddress, setEmailAddress] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSignInPres = async () => {}
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Spinner visible={loading} />
+      <TextInput
+        autoCapitalize="none"
+        placeholder="exemplo@gmail.com"
+        value={emailAddress}
+        onChangeText={setEmailAddress}
+        style={styles.inputField}
+      />
+
+      <TextInput
+        value={password}
+        placeholder="password"
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.inputField}
+      />
+
+      <Button onPress={onSignInPres} title="Login" color={'#6c47ff'} />
+
+      <Link href="/reset" asChild>
+        <Pressable style={styles.button}>
+          <Text>Forgot password?</Text>
+        </Pressable>
+      </Link>
+
+      <Link href="/register" asChild>
+        <Pressable style={styles.button}>
+          <Text>Create Account</Text>
+        </Pressable>
+      </Link>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputField: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#6c47ff',
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: '#fff',
+  },
+  button: {
+    margin: 8,
+    alignItems: 'center',
+  },
+})
